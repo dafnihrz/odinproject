d642184dfc22bae7c31f59936fe3a834d392c611
Dafni <dafni74gx@gmail.com>
2022-09-23 09:47:54 -0400

    Added php files

---
+++ phpbasics/main.php
@@ -0,0 +1,14 @@
+<?php
+
+// Assign value to variable
+$color = "yellow";
+
+// Try to print variable value
+echo "The color of my car is " . $color . "<br>";
+echo "The color of my car is " . $Color . "<br>";
+echo "The color of my car is " . $COLOR . "<br>";
+
+## PART 2
+ECHO "Hello World! <br>";
+echo "Hello World! <br>";
+EcHo "Hello World! <br>";
