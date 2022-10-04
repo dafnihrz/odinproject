61650669c75bd7dfdf6409373296467f0774ed98
Dafni <dafni74gx@gmail.com>
2022-10-04 15:03:03 -0400

    Added links & responsiveness

--- phpbasics/main.php
+++ phpbasics/main.php
@@ -4,11 +4,12 @@
 $color = "yellow";
 
 // Try to print variable value
+//Variables are case sensitive
 echo "The color of my car is " . $color . "<br>";
 echo "The color of my car is " . $Color . "<br>";
 echo "The color of my car is " . $COLOR . "<br>";
 
-## PART 2
+//
 ECHO "Hello World! <br>";
 echo "Hello World! <br>";
 EcHo "Hello World! <br>";
